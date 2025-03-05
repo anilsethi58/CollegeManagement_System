61bad3d7ef1864ce1a18a0cd810b2ebf45b1b6b6
anilsethi58 <136245167+anilsethi58@users.noreply.github.com>
2025-03-05 11:30:54 +0530

    2

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -83,11 +83,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'student_management_system.wsgi.application'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
-    'collegemanagement-system-5.onrender.com'
-]
+ALLOWED_HOSTS = ['*']
+
 
 
 # Database
